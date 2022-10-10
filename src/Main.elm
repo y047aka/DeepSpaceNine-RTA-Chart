c5866cf7610ee497aaf52b0f2aae6bf09d9f7222
Yoshitaka Totsuka <y047aka@gmail.com>
2022-10-10 15:44:50 +0900

    Update view

--- src/Main.elm
+++ src/Main.elm
@@ -75,32 +75,45 @@ episodeView : Int -> Episode -> Html msg
 episodeView index { season, episode, title, title_ja, importance, netflix_id } =
     tr
         [ css
-            [ backgroundColor (hsl 175 0.6 (backgroundByImportance importance))
+            [ height (px 31)
+            , fontSize (px 10)
+            , color (hsl 175 0.6 (stepByImportance importance))
             , children
                 [ Global.selector "td:not(:last-child)" [ padding2 (px 5) (px 10) ] ]
             ]
         ]
-        [ td [ css [ textAlign center, fontSize (px 14) ] ] [ text <| String.fromInt (index + 1) ]
+        [ td [ css [ textAlign center, fontSize (px 12) ] ] [ text <| String.fromInt (index + 1) ]
         , td [ css [ textAlign center ] ]
-            [ div [ css [ fontSize (px 10) ] ] [ text <| "Season " ++ String.fromInt season ]
-            , div [ css [ fontSize (px 12) ] ] [ text <| "" ++ String.fromInt episode ]
+            [ text <| "S" ++ String.fromInt season ++ " - E" ++ String.fromInt episode ]
+        , td
+            [ css
+                [ before
+                    [ property "content" "''"
+                    , display block
+                    , margin auto
+                    , width <| px (toFloat importance * 3)
+                    , height <| px (toFloat importance * 3)
+                    , backgroundColor currentColor
+                    , borderRadius (px 50)
+                    ]
+                ]
             ]
+            []
+        , td [ css [ fontSize <| px (toFloat importance + 10) ] ] [ text title ]
+        , td [] [ text title_ja ]
         , td []
-            [ text title
-            , div [ css [ fontSize (px 10) ] ] [ text title_ja ]
-            ]
-        , td [ css [ property "display" "contents" ] ]
             [ a
                 [ href <| "https://www.netflix.com/watch/" ++ String.fromInt netflix_id
                 , Attributes.target "_blank"
                 , css
                     [ display tableCell
-                    , padding2 zero (px 10)
-                    , verticalAlign middle
+                    , padding2 (px 0) (px 15)
+                    , fontSize (px 12)
                     , textDecoration none
-                    , backgroundColor (hsl 350 0.5 0.5)
-                    , color inherit
-                    , visited [ backgroundColor (hsl 350 0.5 0.2) ]
+                    , backgroundColor (hsl 350 0.5 0.4)
+                    , color (hex "#000")
+                    , borderRadius (px 15)
+                    , visited [ backgroundColor (hsl 350 0.5 0.15) ]
                     ]
                 ]
                 [ text "NETFLIX" ]
@@ -108,6 +121,14 @@ episodeView index { season, episode, title, title_ja, importance, netflix_id } =
         ]
 
 
-backgroundByImportance : Int -> Float
-backgroundByImportance importance =
-    importance |> toFloat |> (*) 0.1 |> (+) 0.2
+stepByImportance : Int -> Float
+stepByImportance importance =
+    if importance > 3 then
+        0.5
+        -- importance |> toFloat |> (*) 0.15
+
+    else if importance > 2 then
+        0.3
+
+    else
+        0.15
