72dce33837d462bde3cea01683a6730ce13835e8
Yoshitaka Totsuka <y047aka@gmail.com>
2023-06-06 22:26:10 +0900

    refactor

--- src/Main.elm
+++ src/Main.elm
@@ -67,8 +67,8 @@ update msg model =
 view : Model -> Html Msg
 view { episodes, afterSeason4 } =
     let
-        characters =
-            characters_ afterSeason4
+        characters_ =
+            config afterSeason4
     in
     div []
         [ global [ Css.Global.body [ backgroundColor (hsl 0 0 0.1), color (hsl 0 0 0.6) ] ]
@@ -78,43 +78,30 @@ view { episodes, afterSeason4 } =
             , text "Show characters after season 4"
             ]
         , Html.table [ css [ margin2 zero auto, borderCollapse collapse ] ] <|
-            List.indexedMap (episodeView characters) episodes
+            List.indexedMap (episodeView characters_) episodes
         ]
 
 
-characters_ : Bool -> List Character
-characters_ afterSeason4 =
-    [ BenjaminSisko, JakeSisko, Dax, KiraNerys, MilesObrien, KeikoObrien, Bashir, Odo, Quark, Rom, Nog, Winn, Bareil, Garak, Dukat ]
-        ++ (if afterSeason4 then
-                [ MichaelEddington, KasidyYates, Leeta, Worf, Gowron, Martok, Shakaar, Ziyal, Damar ]
+config : Bool -> { characters : List Character }
+config afterSeason4 =
+    { characters =
+        [ BenjaminSisko, JakeSisko, Dax, KiraNerys, MilesObrien, KeikoObrien, Bashir, Odo, Quark, Rom, Nog, Winn, Bareil, Garak, Dukat ]
+            ++ (if afterSeason4 then
+                    [ MichaelEddington, KasidyYates, Leeta, Worf, Gowron, Martok, Shakaar, Ziyal, Damar ]
 
-            else
-                []
-           )
+                else
+                    []
+               )
+    }
 
 
-episodeView : List Character -> Int -> Episode -> Html msg
-episodeView characters index ({ season, episode, title, title_ja, importance, netflix_id } as ep) =
-    let
-        characterDict =
-            ep.characters
-                |> List.map (\{ name, contrast } -> ( name, contrast ))
-                |> Dict.fromList
-
-        charactersContrastColumns =
-            List.map
-                (\character ->
-                    Dict.get (Character.toString character) characterDict
-                        |> Maybe.map (\contrast -> td [ css [ color (hsl 0 0 (stepByImportance contrast)) ] ] [ text (Character.toString character) ])
-                        |> Maybe.withDefault (td [] [])
-                )
-                characters
-    in
+episodeView : { characters : List Character } -> Int -> Episode -> Html msg
+episodeView config_ index ep =
     tr
         [ css
             [ height (px 31)
             , fontSize (px 10)
-            , color (hsl 0 0 (stepByImportance importance))
+            , color (hsl 0 0 (stepByImportance ep.importance))
             , nthChild "odd"
                 [ backgroundColor (hsl 0 0 0.095) ]
             , transition
@@ -124,48 +111,72 @@ episodeView characters index ({ season, episode, title, title_ja, importance, ne
             , hover [ backgroundColor (hsl 0 0 0.15) ]
             ]
         ]
-    <|
-        [ td [ css [ textAlign center, fontSize (px 12) ] ] [ text <| String.fromInt (index + 1) ]
-        , td [ css [ minWidth (em 4.5), textAlign center ] ]
-            [ text <| "S" ++ String.fromInt season ++ " - E" ++ String.fromInt episode ]
-        , td
-            [ css
-                [ before
-                    [ property "content" "''"
-                    , display block
-                    , margin auto
-                    , width <| px (toFloat importance * 5 - 2)
-                    , height <| px (toFloat importance * 5 - 2)
-                    , backgroundColor currentColor
-                    , borderRadius (px 50)
-                    ]
+        (episodeSummaryColumns index ep
+            ++ charactersContrastColumns config_ ep
+            ++ [ td [] [ netflixLink ep.netflix_id ] ]
+        )
+
+
+charactersContrastColumns : { characters : List Character } -> Episode -> List (Html msg)
+charactersContrastColumns config_ ep =
+    let
+        characterDict =
+            ep.characters
+                |> List.map (\{ name, contrast } -> ( name, contrast ))
+                |> Dict.fromList
+    in
+    List.map
+        (\character ->
+            Dict.get (Character.toString character) characterDict
+                |> Maybe.map (\contrast -> td [ css [ color (hsl 0 0 (stepByImportance contrast)) ] ] [ text (Character.toString character) ])
+                |> Maybe.withDefault (td [] [])
+        )
+        config_.characters
+
+
+episodeSummaryColumns : Int -> Episode -> List (Html msg)
+episodeSummaryColumns index { season, episode, title, title_ja, importance } =
+    [ td [ css [ textAlign center, fontSize (px 12) ] ] [ text <| String.fromInt (index + 1) ]
+    , td [ css [ minWidth (em 4.5), textAlign center ] ]
+        [ text <| "S" ++ String.fromInt season ++ " - E" ++ String.fromInt episode ]
+    , td
+        [ css
+            [ before
+                [ property "content" "''"
+                , display block
+                , margin auto
+                , width <| px (toFloat importance * 5 - 2)
+                , height <| px (toFloat importance * 5 - 2)
+                , backgroundColor currentColor
+                , borderRadius (px 50)
                 ]
             ]
-            []
-        , td [ css [ displayFlex, flexDirection column, property "row-gap" "2px" ] ]
-            [ div [ css [ fontSize (px 12) ] ] [ text title ]
-            , div [ css [ color (hsl 0 0 0.4) ] ] [ text title_ja ]
+        ]
+        []
+    , td [ css [ displayFlex, flexDirection column, property "row-gap" "2px" ] ]
+        [ div [ css [ fontSize (px 12) ] ] [ text title ]
+        , div [ css [ color (hsl 0 0 0.4) ] ] [ text title_ja ]
+        ]
+    ]
+
+
+netflixLink : Int -> Html msg
+netflixLink netflix_id =
+    a
+        [ href <| "https://www.netflix.com/watch/" ++ String.fromInt netflix_id
+        , Attributes.target "_blank"
+        , css
+            [ display tableCell
+            , padding2 (px 0) (px 10)
+            , fontSize (px 12)
+            , textDecoration none
+            , backgroundColor (hsl 350 0.5 0.45)
+            , color (hsl 0 0 0.1)
+            , borderRadius (px 15)
+            , visited [ backgroundColor (hsl 350 0.5 0.2) ]
             ]
         ]
-            ++ charactersContrastColumns
-            ++ [ td []
-                    [ a
-                        [ href <| "https://www.netflix.com/watch/" ++ String.fromInt netflix_id
-                        , Attributes.target "_blank"
-                        , css
-                            [ display tableCell
-                            , padding2 (px 0) (px 10)
-                            , fontSize (px 12)
-                            , textDecoration none
-                            , backgroundColor (hsl 350 0.5 0.45)
-                            , color (hsl 0 0 0.1)
-                            , borderRadius (px 15)
-                            , visited [ backgroundColor (hsl 350 0.5 0.2) ]
-                            ]
-                        ]
-                        [ text "NETFLIX" ]
-                    ]
-               ]
+        [ text "NETFLIX" ]
 
 
 stepByImportance : Int -> Float
