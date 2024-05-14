df1f0dfd9d337cb085edb5858d637f2dacd3391a
Yoshitaka Totsuka <y047aka@gmail.com>
2024-05-14 23:09:07 +0900

    refactor

--- app/src/Data/Episode.elm
+++ app/src/Data/Episode.elm
@@ -24,23 +24,53 @@ episodesDecoder =
 
 episodeDecoder : Decode.Decoder Episode
 episodeDecoder =
-    Decode.map8 Episode
+    let
+        fromStringWith fromString_ nac =
+            fromString_ nac.name
+                |> Maybe.map (\name -> { name = name, contrast = nac.contrast })
+    in
+    Decode.map
+        (\d ->
+            { season = d.season
+            , episode = d.episode
+            , title = d.title
+            , title_ja = d.title_ja
+            , importance = d.importance
+            , netflix_id = d.netflix_id
+            , characters = List.filterMap (fromStringWith Character.fromString) d.characters
+            , organizations = List.filterMap (fromStringWith Organization.fromString) d.organizations
+            }
+        )
+        primitiveDecoder
+
+
+type alias Episode_Primitive =
+    { season : Int
+    , episode : Int
+    , title : String
+    , title_ja : String
+    , importance : Int
+    , netflix_id : Int
+    , characters : List NameAndContrast
+    , organizations : List NameAndContrast
+    }
+
+
+type alias NameAndContrast =
+    { name : String, contrast : Int }
+
+
+primitiveDecoder : Decode.Decoder Episode_Primitive
+primitiveDecoder =
+    Decode.map8 Episode_Primitive
         (field "season" int)
         (field "episode" int)
         (field "title" string)
         (field "title_ja" string)
         (field "importance" int)
         (field "netflix_id" int)
-        (field "characters" (Decode.list nameAndContrastDecoder)
-            |> Decode.map (List.filterMap (\c -> Character.fromString c.name |> Maybe.map (\c_ -> { name = c_, contrast = c.contrast })))
-        )
-        (field "organizations" (Decode.list nameAndContrastDecoder)
-            |> Decode.map (List.filterMap (\o -> Organization.fromString o.name |> Maybe.map (\o_ -> { name = o_, contrast = o.contrast })))
-        )
-
-
-type alias NameAndContrast =
-    { name : String, contrast : Int }
+        (field "characters" (Decode.list nameAndContrastDecoder))
+        (field "organizations" (Decode.list nameAndContrastDecoder))
 
 
 nameAndContrastDecoder : Decode.Decoder NameAndContrast
